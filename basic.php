979b3b800e6c1738a311a5742bed4563b77a1aef
IMAM FAHMI FADILLAH <imam.fahmi29@gmail.com>
2017-11-04 18:30:50 +0700

    basic.php
    Design Terbaru

---
+++ basic.php
@@ -0,0 +1,23 @@
+<html> 
+	<head> 
+		<title>PHP Test</title> 
+	</head> 
+	<body> 
+	<?php echo '<p>Hello World!</p>'; ?><br>
+
+	<?php
+	echo 'Hello World!<br />';
+	echo('Hello World!<br />');
+	print 'Hello World!<br />';
+	print('Hello World!<br />');
+	?>
+
+	<?php
+	// Claire O'Reilly said "Hello".
+	echo 'Claire O\'Reilly';
+	echo "said \"Hello\"."; 
+	?>
+
+
+	</body> 
+</html>
